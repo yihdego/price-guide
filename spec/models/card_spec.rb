35c943902b725caa478cdf40cbaae53e5855f2d6
Amanuel <yihdego@msn.com>
2017-12-04 17:42:25 -0800

    card attributes tested

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe Card, type: :model do
   let!(:jace) { create(:card) }
   describe 'attributes' do
     it 'has a name' do
-      expect(jace.name).to eq ''
+      expect(jace.name).to eq "Jace, Vryn's Prodigy"
     end
     it 'has a set' do
-      expect(jace.set).to eq ''
+      expect(jace.set).to eq "Magic Origins"
     end
     it 'has a game' do
-      expect(jace.game).to eq ''
+      expect(jace.game).to eq "Magic"
     end
   end
 end
